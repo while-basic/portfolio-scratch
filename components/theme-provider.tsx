f9389b8e0b70008aaa8e9e901e12c5175e5a4888
while-basic <c_celaya@icloud.com>
2024-11-29 20:23:05 -0700

    fix: update ThemeProvider types
    - Import and extend NextThemeProviderProps
    - Properly type the attribute prop
    - Add default value for attribute

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,19 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps as NextThemeProviderProps } from "next-themes/dist/types"
 
-interface ThemeProviderProps {
+interface ThemeProviderProps extends Omit<NextThemeProviderProps, "attribute"> {
   children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  forcedTheme?: string
-  disableTransitionOnChange?: boolean
+  attribute?: "class" | "data-theme"
 }
 
 export function ThemeProvider({ 
   children,
+  attribute = "class",
   ...props
 }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider attribute={attribute} {...props}>{children}</NextThemesProvider>
 }
